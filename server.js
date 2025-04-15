8c44e673e0e42703516c4e367047ef2510967803
pack <chanisaraaaaaiii@gmail.com>
2025-04-15 21:55:56 +0700

    add data

--- server.js
+++ server.js
@@ -17,22 +17,22 @@ const courses = [
 
 
 
-const nutrition = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "nutrition",
-  password: "23082539",
-  port: 5432,
-});
-
-
 // const nutrition = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_DATABASE,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
+//   user: "postgres",
+//   host: "localhost",
+//   database: "nutrition",
+//   password: "23082539",
+//   port: 5432,
 // });
+
+
+const nutrition = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 
 nutrition.connect()
